1dc1034bcab7cfbd01b18b96b18e1d91a120e3c5
zhid0399123 <zhid0399123@gmail.com>
2024-06-18 23:16:27 +0300

    schema loader
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/schemas/schemas-loader.js
+++ src/schemas/schemas-loader.js
@@ -1,13 +1,13 @@
-import fastifyPlugin from 'fastify-plugin'
+import plugin from 'fastify-plugin'
 import authRegisterSchema from './auth/auth-register-body.json' assert { type: "json" }
 import authTokenResponseSchema from './auth/auth-token-response.json' assert { type: "json" }
 import authRefreshTokenHeaderSchema from './auth/auth-refresh-token-header.json' assert { type: "json" }
 import dotEnvConfSchema from './dotenv/dotenv.json' assert { type: "json" }
 
-export default fastifyPlugin(async function schemasLoader(fastify, opts, next) {
-  fastify.addSchema(dotEnvConfSchema)
-  fastify.addSchema(authRegisterSchema)
-  fastify.addSchema(authTokenResponseSchema)
-  fastify.addSchema(authRefreshTokenHeaderSchema)
+export default plugin(async function schemasLoader(app, opts, next) {
+  app.addSchema(dotEnvConfSchema)
+  app.addSchema(authRegisterSchema)
+  app.addSchema(authTokenResponseSchema)
+  app.addSchema(authRefreshTokenHeaderSchema)
   next()
 })
