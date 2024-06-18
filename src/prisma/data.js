445d149a8f03b4d5145090eaa752fbf8bc867cea
zhid0399123 <zhid0399123@gmail.com>
2024-06-18 23:09:48 +0300

    data
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

---
+++ src/prisma/data.js
@@ -0,0 +1,71 @@
+export default [
+  {
+    name: 'Robert Martin',
+    born: 1952,
+    books: [
+      {
+        title: 'Clean Code',
+        published: 2008,
+        genres: ['refactoring']
+      },
+      {
+        title: 'Agile software development',
+        published: 2002,
+        genres: ['agile', 'patterns', 'design']
+      },
+    ]
+  },
+  {
+    name: 'Martin Fowler',
+    born: 1963,
+    books: [
+      {
+        title: 'Refactoring, edition 2',
+        published: 2018,
+        genres: ['refactoring']
+      },
+    ]
+  },
+  {
+    name: 'Fyodor Dostoevsky',
+    born: 1821,
+    books: [
+      {
+        title: 'Refactoring to patterns',
+        published: 2008,
+        genres: ['refactoring', 'patterns']
+      },
+      {
+        title: 'Crime and punishment',
+        published: 1866,
+        genres: ['classic', 'crime']
+      },
+      {
+        title: 'Demons',
+        published: 1872,
+        genres: ['classic', 'revolution']
+      },
+    ]
+  },
+  {
+    name: 'Sandi Metz',
+    books: [
+      {
+        title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+        published: 2012,
+        genres: ['refactoring', 'design']
+      },
+    ]
+  },
+  {
+    name: 'James G. Speight',
+    books: [
+      {
+        title: 'Petroleum Chemistry And Refining',
+        published: 1997,
+        genres: ["chemisry", "petroleum"]
+      }
+    ]
+  },
+]
+
