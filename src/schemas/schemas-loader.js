946076254a685c12ec0b550b315759e1c4ead01c
zhid0399123 <zhid0399123@gmail.com>
2024-06-19 20:33:10 +0300

    removed auth-schemas
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/schemas/schemas-loader.js
+++ src/schemas/schemas-loader.js
@@ -1,15 +1,9 @@
 import plugin from 'fastify-plugin'
 import dotEnvConfSchema from './dotenv/dotenv.json' assert { type: "json" }
-import authRegisterSchema from './auth/auth-register-body.json' assert { type: "json" }
-import authTokenResponseSchema from './auth/auth-token-response.json' assert { type: "json" }
-import authRefreshTokenHeaderSchema from './auth/auth-refresh-token-header.json' assert { type: "json" }
 
 export default plugin(
   async function schemasLoader(app, _opts, next) {
     app.addSchema(dotEnvConfSchema)
-    app.addSchema(authRegisterSchema)
-    app.addSchema(authTokenResponseSchema)
-    app.addSchema(authRefreshTokenHeaderSchema)
     next()
   }
 )
